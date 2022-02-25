3b17ff1201181bffc4802fa2eba2347c0d467405
Caden <91219328+clafollette303@users.noreply.github.com>
2022-02-25 17:52:36 -0500

    Update Fight.java

--- Fight.java
+++ Fight.java
@@ -2,12 +2,14 @@ import java.util.ArrayList;
 
 public class Fight extends Room
 {
-    private int NUM_ENEMIES;
+    private final int NUM_ENEMIES;
+    private int currentEnemies;
 
     public Fight(String name, String description, ArrayList<Character> characters, int NUM_NPCS, int NUM_ENEMIES)
     {
         super(name, description, characters, NUM_NPCS);
         this.NUM_ENEMIES = NUM_ENEMIES;
+        currentEnemies = NUM_ENEMIES;
     }
 
     public int getNUM_ENEMIES()
@@ -19,6 +21,11 @@ public class Fight extends Room
     {
         this.NUM_ENEMIES = NUM_ENEMIES;
     }
+    
+    public void startFight()
+    {
+        //tells the player that they have entered a fight, and how many enemies they need to defeat
+    }
 
     public void enemyAttack()
     {
@@ -28,5 +35,10 @@ public class Fight extends Room
     public void playerAttack(Weapon weapon)
     {
         //player will attack enemy for a certain amount of damage based on the weapon they are using
+    }
+    
+    public void removeEnemy()
+    {
+        //when an enemies health falls to 0, removes that enemy from the characters array list, and decreases currentEnemies variable by 1.
     }
 }
