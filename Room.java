216b5457c3cdb1f723487df7fd900f2d11de63ea
Caden <91219328+clafollette303@users.noreply.github.com>
2022-02-25 17:27:37 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -4,30 +4,31 @@ public class Room
 {
     protected String name;
     protected String description;
-    protected ArrayList<Character> characters = new ArrayList<Character>();
+    protected ArrayList<Character> characters;
     protected int NUM_NPCS;
 
-    public Room(String name, String description, ArrayList<Character> characters, int NUM_NPCS)
+    public Room(String name, String description, int NUM_NPCS)
     {
         this.name = name;
         this.description = description;
         this.characters = characters;
         this.NUM_NPCS = NUM_NPCS;
+        characters = new ArrayList<Character>();
     }
 
-    public void addCharacter()
+    public void addCharacter(Character c)
     {
-
+        //adds character to array list of characters
     }
 
     public void removeCharacter()
     {
-
+        //removes character from array list if NPC leaves or enemy is killed by player
     }
 
     public void endRoom()
     {
-
+        //used to continue to next section if player leaves area or ends a fight
     }
 
     public String getName()
