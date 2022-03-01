04f8dfb952b22671f387ade2c359187055d7ce2e
Caden <91219328+clafollette303@users.noreply.github.com>
2022-02-28 20:30:41 -0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,17 @@
+public class Game
+{
+    private Player player;
+    private Room[] storyLocations;
+    private Character[] NPCS;
+
+    public Game()
+    {
+        //sets values for game locations and player name
+    }
+
+    public void start()
+    {
+        //used to load game locations
+    }
+
+}
